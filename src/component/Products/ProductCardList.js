c7f4e36ff127afc985cb396c812d452e432eb64e
Looi Wei En <looi.weien02@student.usm.my>
2024-04-06 04:07:35 +0800

    add product details

---
+++ src/component/Products/ProductCardList.js
@@ -0,0 +1,12 @@
+import { products } from "../../provider/productprovider";
+import { ProductCard } from "./ProductCard";
+
+export const ProductCardList = () => {
+  return (
+    <div className="flex flex-row gap-4 my-6 overflow-y-scroll">
+      {products.map((card, index) => (
+        <ProductCard key={index} {...card} />
+      ))}
+    </div>
+  );
+};
